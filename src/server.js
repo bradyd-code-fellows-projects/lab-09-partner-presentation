98b85ac7d921b39b7b7fd5d573ad1e27e9cc7222
Brady <brady.davenport@pm.me>
2022-06-23 16:01:31 -0600

    server and db up and running

---
+++ src/server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+require('dotenv').config();
+
+const errorHandler = require('./middleware/500');
+const notFound = require('./middleware/404');
+
+const app = express();
+
+const PORT = process.env.PORT || 3002;
+
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(notFound);
+app.use(errorHandler);
+
+module.exports = {
+  server: app,
+  start: () => {
+    app.listen(PORT, () => {
+      console.log(`Server Up on ${PORT}`);
+    });
+  },
+};
