681c501ddc0bf2471914cd87a96c75c1c5627f24
Brady <brady.davenport@pm.me>
2022-06-27 09:55:20 -0600

    refactor

--- src/auth/models/users-model.js
+++ src/auth/models/users-model.js
@@ -1,29 +1,29 @@
-// 'use strict';
+'use strict';
 
-// const { Sequelize, DataTypes } = require('sequelize');
-// const jwt = require('jsonwebtoken');
-// const SECRET = process.env.SECRET;
+const { Sequelize, DataTypes } = require('sequelize');
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.SECRET;
 
-// const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_URL);
 
-// const UsersModel = sequelize.define('Users', {
-//   username: {
-//     type: DataTypes.STRING,
-//     allowNull: false,
-//   },
-//   password: {
-//     type: DataTypes.STRING,
-//     allowNull: false,
-//   },
-//   token: {
-//     type: DataTypes.VIRTUAL,
-//     get() {
-//       return jwt.sign({username: this.username}, SECRET, {expiresIn: '8640000'});
-//     },
-//     set(payload){
-//       return jwt.sign(payload, SECRET, {expiresIn: '8640000'});
-//     },
-//   },
-// });
+const UsersModel = sequelize.define('Users', {
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  token: {
+    type: DataTypes.VIRTUAL,
+    get() {
+      return jwt.sign({username: this.username}, SECRET, {expiresIn: '8640000'});
+    },
+    set(payload){
+      return jwt.sign(payload, SECRET, {expiresIn: '8640000'});
+    },
+  },
+});
 
-// module.exports = UsersModel;
+module.exports = UsersModel;
